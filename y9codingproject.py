eccd8815bd79952e18eea0ce3aa145f52bfd22a9
Y9Design-AJSHU <55104759+Y9Design-AJSHU@users.noreply.github.com>
2019-12-06 00:35:48 -0500

    Add files via upload

--- y9codingproject.py
+++ y9codingproject.py
@@ -34,15 +34,16 @@ for i in range(0, len(data),1):
 # BELOW CALCULATES THE CALORIC GOAL----------------------------------------
 
 def goalCalsCalc():
+
 	bw=data[len(data)-1]
 	heightFile2 = open("height.txt", "r")
-	height = float(heightFile2.read())
+	height = float(str(heightFile2.read()))
 	ageFile2 = open("age.txt", "r")
-	age = float(ageFile2.read())
+	age = float(str(ageFile2.read()))
 	mORfFile2 = open("mORf.txt", "r")
 	mORf = mORfFile2.read()
 	alFile2 = open("al.txt", "r")
-	al = int(alFile2.read())
+	al = int(str(alFile2.read()))
 
 	def mancalc(bw, h, a, al):
 		calc1 = (66+(6.3*bw)+(12.9*h)-(6.8*a))
@@ -115,40 +116,47 @@ def funFact():
 	
 def submitStats():
 	#This function writes all the info entered in the first window into their individual .txt files
-	with open("height.txt", "w") as myfile:
-		myfile.write(str(heightEntry1.get()))
-	with open("al.txt", "w") as myfile:
-		myfile.write(str(aLEntry1.get()))
-	with open("age.txt", "w") as myfile:
-		myfile.write(str(ageEntry1.get()))
-	with open("data.txt", "a") as myfile:
-		myfile.write("\n"+str(weightEntry1.get()))
-	with open("mORf.txt", "w") as myfile:
-		myfile.write(str(gEntry1.get()))
-
-	root2.destroy()
-
+	try:
+		with open("height.txt", "w") as myfile:
+			myfile.write(str(float(heightEntry1.get())))
+		with open("al.txt", "w") as myfile:
+			myfile.write(str(int(aLEntry1.get())))
+		with open("age.txt", "w") as myfile:
+			myfile.write(str(float(ageEntry1.get())))
+		with open("data.txt", "a") as myfile:
+			myfile.write("\n"+str(float(weightEntry1.get())))
+		with open("mORf.txt", "w") as myfile:
+			myfile.write(str(gEntry1.get()))
+		root2.destroy()
+	except ValueError:
+		print("Please enter the right type!")
+	
 def weigthRunMe():
 	'''This function adds the weight entered into the data list. It will replace the last item in the list if it is the same day and it
 	will create a new item if it is the same day.'''
 	#Step 2:  Access and then clear widget
 	value = weightEntry.get()
 	weightEntry.delete(0, 'end')
-	#Step 3: Append value to the list
-	c.delete("all")
-	dayFile = open("day1.txt", "r")
-	day2int = int(dayFile.read())		
-	if day==day2int:
-		data.pop(len(data)-1)
-		data.insert(len(data),float(value))	
-	elif day!=day2int:
-		dayFile2 = open("day1.txt", "w")
-		dayFile2.write(str(day))
-		dayFile2.close()
-		data.append(float(value))
-	#Step 4: Update display
-	chartUpdate()
-	
+	try:
+		if float(value)>0:
+			#Step 3: Append value to the list
+			c.delete("all")
+			dayFile = open("day1.txt", "r")
+			day2int = int(dayFile.read())		
+			if day==day2int:
+				data.pop(len(data)-1)
+				data.insert(len(data),float(value))	
+			elif day!=day2int:
+				dayFile2 = open("day1.txt", "w")
+				dayFile2.write(str(day))
+				dayFile2.close()
+				data.append(float(value))
+			#Step 4: Update display
+			chartUpdate()
+		else:
+			print("Enter a positive number!")
+	except ValueError:
+		print("Enter a number!")
 	
 def chartUpdate():
 	#This function updates the chart when a new weight is entered.
@@ -166,7 +174,7 @@ def chartUpdate():
 		x1 = ctr * x_stretch + ctr * x_width + x_width + x_gap
 		y1 = 220 - y_gap
 		c.create_rectangle(x0-18, y0, x1-18, y1, fill="#ff8f00")
-		c.create_text(x0-15, y0, anchor='sw', text=str(int(data[i])), font=("Purisa", 18), fill='white')
+		c.create_text(x0-15, y0, anchor='sw', text=str(int(data[i])), font=("System", 18), fill='white')
 		ctr = ctr + 1
 #Step 1: Bind this function to an action	
 
@@ -174,15 +182,21 @@ def calsRunMe():
 	#This function updates the bar value and label text when calories are entered.
 	global cals
 	#Step 2:  Access widget
-	value1 = float(calsEntry.get())
-	
-	#Step 3: Add to Variable
-	cals+=value1
-	#Step 4: Update display	
-	bar['value'] = float(cals)/float(calsGoal)*100
-	barValue = float(cals)/float(calsGoal)*100
-	label['text'] = str(int(round(barValue,0)))+'%'
-	calsEntry.delete(0, 'end')
+	try:
+		value1 = float(calsEntry.get())
+		if value1>0:
+			#Step 3: Add to Variable
+			cals+=value1
+			#Step 4: Update display	
+			bar['value'] = float(cals)/float(calsGoal)*100
+			barValue = float(cals)/float(calsGoal)*100
+			label['text'] = str(int(round(barValue,0)))+'%'
+			calsEntry.delete(0, 'end')
+		else:
+			print("Enter a positive number!")
+	except ValueError:
+		print("Please enter a number!")
+		calsEntry.delete(0, 'end')
 
 
 def on_closing():
@@ -211,16 +225,16 @@ style2.theme_use('default')
 dayFunc()
 #styling
 style2.configure("2orange.horizontal.TEntry", foreground='#ff8f00')
-style2.configure("2orange.TButton", background='#ff8f00', font=("Purisa", 20))
-style2.configure("transp.TLabel", font=("Purisa", 20), foreground='black', background='white')
-style2.configure("transp1.TLabel", font=("Purisa", 10), foreground='black', background='white')
+style2.configure("2orange.TButton", background='#ff8f00', font=("System", 20))
+style2.configure("transp.TLabel", font=("System", 20), foreground='black', background='white')
+style2.configure("transp1.TLabel", font=("System", 10), foreground='black', background='white')
 
 warning=Label(root2, text='If you have already entered this info, just exit this window and the next window will pop up.', style='transp1.TLabel')
 warning.grid(row=1, column=1, padx = 10, pady=10, columnspan=2)
 
 
 #Age
-ageEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("Purisa", 20))
+ageEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("System", 20))
 ageEntry1.grid(row=3, column=1, padx = 10, pady=10)
 
 ageEntry1Label=Label(root2, text='Enter Age Here', style='transp.TLabel')
@@ -228,28 +242,28 @@ ageEntry1Label.grid(row=2, column=1, padx = 10, pady=10)
 
 
 #Height
-heightEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("Purisa", 20))
+heightEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("System", 20))
 heightEntry1.grid(row=3, column=2, padx = 10, pady=10)
 
 heightEntry1Label=Label(root2, text='Enter Height Here (in)', style='transp.TLabel')
 heightEntry1Label.grid(row=2, column=2, padx = 10, pady=10)
 
 #Gender
-gEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("Purisa", 20))
+gEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("System", 20))
 gEntry1.grid(row=7, column=1, padx = 10, pady=10)
 
 gEntry1Label=Label(root2, text='Male or Female (F or M)?', style='transp.TLabel')
 gEntry1Label.grid(row=6, column=1, padx = 10, pady=10)
 					
 #Activity Level
-aLEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("Purisa", 20))
+aLEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("System", 20))
 aLEntry1.grid(row=5, column=1, padx = 10, pady=10)
 
 aLEntry1Label=Label(root2, text='Activity Level - 1-5', style='transp.TLabel')
 aLEntry1Label.grid(row=4, column=1, padx = 10, pady=10)
 
 #Weight
-weightEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("Purisa", 20))
+weightEntry1 = Entry(root2, width=15,style='2orange.horizontal.TEntry',font=("System", 20))
 weightEntry1.grid(row=5, column=2, padx = 10, pady=10)
 
 weightEntry1Label=Label(root2, text='Enter Weight Here (lbs.)', style='transp.TLabel')
@@ -270,10 +284,10 @@ style.theme_use('default')
 
 #styling
 style.configure("orange.horizontal.TEntry", foreground='#ff8f00')
-style.configure("orange.TButton", background='#ff8f00', font=("Purisa", 20))
+style.configure("orange.TButton", background='#ff8f00', font=("System", 20))
 style.configure("orange.Horizontal.TProgressbar", background='#ff8f00')
-style.configure("transp.TLabel", font=("Purisa", 20), foreground='black' )
-style.configure("norm.TLabel", font=("Purisa", 20), foreground='black', bg='white')
+style.configure("transp.TLabel", font=("System", 20), foreground='black' )
+style.configure("norm.TLabel", font=("System", 20), foreground='black', bg='white')
 
 #canvas
 c = Canvas(root, background='#8b0000', highlightthickness=3, highlightbackground="black", width=390)
@@ -296,7 +310,7 @@ weightButton = Button(root, text="Submit Weight", style='orange.TButton', comman
 weightButton.grid(row=2, column=1)
 
 #Type your weight here
-weightEntry = Entry(root, width=15,style='orange.horizontal.TEntry',font=("Purisa", 20))
+weightEntry = Entry(root, width=15,style='orange.horizontal.TEntry',font=("System", 20))
 weightEntry.grid(row=1, column=1, padx = 10, pady=10)
 
 
@@ -306,7 +320,7 @@ calsButton = Button(root, text="Submit Calories", style='orange.TButton', comman
 calsButton.grid(row=2, column=3)
 
 #Type your cals here
-calsEntry = Entry(root, width=15,style='orange.horizontal.TEntry',font=("Purisa", 20))
+calsEntry = Entry(root, width=15,style='orange.horizontal.TEntry',font=("System", 20))
 calsEntry.grid(row=1, column=3, padx=10, pady=10);
 
 funFact=Label(root,text = funFact(), style = 'norm.TLabel', wraplength=170)
